f5e9eaf001a6986f70fba66afc8396c907dc9d22
Kenton Duprey <kdtech18@gmail.com>
2020-11-14 02:46:51 -0500

    added colour

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 import "./css/main.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+// import axios from "axios";
 
 const App = () => {
-	const [dogPic, setDogPic] = useState("");
-	const dogs = async () => {
-		const randomPic = Math.floor(Math.random() * 10);
-		const randomPage = Math.floor(Math.random() * 100);
-		const response = await axios.get(
-			"https://api.unsplash.com/search/photos",
-			{
-				headers: {
-					"Accept-Version": "v1",
-					Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
-				},
-				params: {
-					page: randomPage,
-					query: "puppy",
-				},
-			}
-		);
+	// const [dogPic, setDogPic] = useState("");
+	// const dogs = async () => {
+	// 	const randomPic = Math.floor(Math.random() * 10);
+	// 	const randomPage = Math.floor(Math.random() * 100);
+	// 	const response = await axios.get(
+	// 		"https://api.unsplash.com/search/photos",
+	// 		{
+	// 			headers: {
+	// 				"Accept-Version": "v1",
+	// 				Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+	// 			},
+	// 			params: {
+	// 				page: randomPage,
+	// 				query: "puppy",
+	// 			},
+	// 		}
+	// 	);
 
-		setDogPic(response.data.results[randomPic].urls.regular);
-	};
+	// 	setDogPic(response.data.results[randomPic].urls.regular);
+	// };
 
 	useEffect(() => {
 		//dogs();
 	}, []);
 
 	return (
-		<div className="container mx-auto h-screen flex items-center justify-center sm:flex-row">
-			<div className="flex-1 text-center p-2">
+		<div className="container mx-auto h-auto pt-8 md:h-screen flex items-center justify-center flex-col md:flex-row">
+			<div className="flex-shrink text-center px-2 pb-4 flex items-center justify-center flex-col">
 				<h1 className="text-4xl text-white">
 					Hi! My name is{" "}
-					<span className="dark-purple">Kenton</span>,
+					<span className="light-cyan">Kenton</span>,
 					<br /> I make web apps.
 				</h1>
 				<h3 className="text-2xl text-white">
-					My site is not fully ready, but enjoy a random picture
-					of a puppy in the meantime 🐶
+					My site is <span className="dark-red">not</span> fully
+					ready, but enjoy a random picture of a{" "}
+					<span className="dark-cyan">puppy</span> in the
+					meantime 🐶
 				</h3>
 			</div>
-			<div className="flex-none w-1/2">
+			<div className="flex-none md:w-1/2">
 				<img
 					className="rounded-md shadow-xl bg-white"
 					src="https://images.unsplash.com/photo-1579180728061-3a0f2d871a31?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEzODc3NH0"
