49ed7807aa3499feb264b02f8e648497eeca5c66
Kenton Duprey <kentonduprey@icloud.com>
2021-11-03 01:40:55 -0400

    updating navbar layout

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
 				<a className="navbar-brand" href="#">
 					Kenton D. Web Developer
 				</a>
-				<div>
+				<div className="d-flex flex-column align-items-end">
 					<button
-						className="navbar-toggler"
+						className="navbar-toggler border-0 px-0"
 						type="button"
 						data-bs-toggle="collapse"
 						data-bs-target="#navbarLinks"
@@ -19,19 +19,20 @@ const Navbar = () => {
 					>
 						<span className="navbar-toggler-icon"></span>
 					</button>
+
 					<div className="collapse navbar-collapse" id="navbarLinks">
 						<div className="navbar-nav">
 							<a
-								className="nav-link active"
+								className="nav-link active text-end"
 								aria-current="true"
 								href="#"
 							>
 								Home
 							</a>
-							<a className="nav-link" href="#">
-								See My work
+							<a className="nav-link text-end" href="#">
+								See My Work
 							</a>
-							<a className="nav-link text-primary ml-lg-3">
+							<a className="nav-link text-primary ml-lg-3 text-end">
 								Contact Me
 							</a>
 						</div>
