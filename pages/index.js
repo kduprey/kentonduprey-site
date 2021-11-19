3cd7741c46074118cbd1a1710a7a9ce839f3de86
Kenton Duprey <kentonduprey@icloud.com>
2021-11-19 11:48:10 +0000

    updated site title

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Home = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			{/* Header */}
-			<Header title="Next.js Tailwind Template" />
+			<Header title="Kenton Duprey - Web Developer" />
 
 			<main>
 				<h1 className="text-6xl font-bold">
