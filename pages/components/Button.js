455081a5627ab60c6d7b4f3699486b1409d96c40
Kenton Duprey <kentonduprey@icloud.com>
2021-11-19 21:59:39 +0000

    removing inline-flex from button

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ text, children }) => {
 	return (
-		<button className="m-1 font-display  text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0">
+		<button className="m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0">
 			{text}
 			{children}
 		</button>
