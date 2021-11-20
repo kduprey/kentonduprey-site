c854b9828d6c2010acc657e503925fe16b9ff027
Kenton Duprey <kentonduprey@icloud.com>
2021-11-20 00:32:58 +0000

    adding terminary operator for children on button

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -2,7 +2,7 @@ const Button = ({ text, children }) => {
 	return (
 		<button className="flex flex-row justify-evenly m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0">
 			{text}
-			<div className="p-1">{children}</div>
+			{children ? <div className="">{children}</div> : null}
 		</button>
 	);
 };
