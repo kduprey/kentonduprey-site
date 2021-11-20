8ad6879c5b8ed6600b68a7820139956d22bca04d
Kenton Duprey <kentonduprey@icloud.com>
2021-11-20 13:21:42 +0000

    removing top margin

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -3,7 +3,7 @@ const Button = ({ className, text, children }) => {
 		<button
 			className={
 				className +
-				" flex flex-row justify-evenly m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0"
+				" flex flex-row justify-evenly m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base"
 			}
 		>
 			{text}
