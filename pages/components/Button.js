315c99dcb9bfadbcff88fd9e39e31668b914eb0a
Kenton Duprey <kentonduprey@icloud.com>
2021-11-20 00:27:36 +0000

    adding flexbox to button

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,8 +1,8 @@
 const Button = ({ text, children }) => {
 	return (
-		<button className="m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0">
+		<button className="flex flex-row justify-evenly m-1 font-display text-teal-600 items-center bg-teal-100 border-0 py-1 px-3 focus:outline-none hover:bg-teal-200 rounded text-base mt-4 md:mt-0">
 			{text}
-			{children}
+			<div className="p-1">{children}</div>
 		</button>
 	);
 };
