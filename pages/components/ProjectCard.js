ae596f9bb8e378f1f389926dd9a5f756ed0509f9
Kenton Duprey <kentonduprey@icloud.com>
2021-11-22 15:01:35 +0000

    adding responsive width to projectcards

--- pages/components/ProjectCard.js
+++ pages/components/ProjectCard.js
@@ -10,7 +10,7 @@ const ProjectCard = ({ image, title, blurb, link, skills }) => {
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni explicabo ipsum quas tenetur libero repellat, quos dignissimos consectetur harum a.";
 
 	return (
-		<div className="flex flex-col justify-center p-3">
+		<div className="flex flex-col justify-center p-3 md:w-1/2">
 			<div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
 				<div className="w-full md:w-1/3 bg-white grid place-items-center">
 					<img src={image} alt={title} className="rounded-xl" />
