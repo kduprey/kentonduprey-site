3320d6ec69fa7f6d4c2b812790177c5fddacf565
Kenton Duprey <kentonduprey@icloud.com>
2021-11-22 00:31:58 +0000

    imported new components for edits

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProjectCard from "./components/ProjectCard";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Contact from "./components/Contact";
 
 const Home = () => {
 	return (
@@ -11,6 +15,28 @@ const Home = () => {
 			<main className="font-display vh-full flex flex-col items-center">
 				<Navbar />
 				<Hero />
+				<section className="container">
+					<h1 className="text-4xl font-bold text-center py-3">
+						Work
+					</h1>
+					<div className="flex flex-col md:flex-row justify-evenly items-center">
+						<ProjectCard
+							image="/projects/worldeyef-proj.jpeg"
+							title="World Eye Foundation"
+							link="https://dev.worldeyef.org"
+						/>
+						<ProjectCard />
+						<ProjectCard />
+					</div>
+					<About />
+					<section>
+						<h1 className="text-5xl font-bold text-center py-3">
+							Skills
+						</h1>
+						<Skill />
+					</section>
+					<Contact />
+				</section>
 			</main>
 		</div>
 	);
