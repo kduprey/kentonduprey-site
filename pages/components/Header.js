b153f7ed5f0b8f588704f2a1ef5da521212981ca
Kenton Duprey <kentonduprey@icloud.com>
2021-11-22 18:02:12 +0000

    adding google analytics

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,6 +3,24 @@ import Head from "next/head";
 const Header = ({ title }) => {
 	return (
 		<Head>
+			<script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-47RFWE9Y3T"
+			></script>
+			<script>
+				dangerouslySetInnerHTML=
+				{{
+					__html: `
+  					window.dataLayer = window.dataLayer || [];
+  					function gtag(){dataLayer.push(arguments);}
+  					gtag('js', new Date());
+
+  					gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+						  page_path: window.location.pathname,
+						  });
+  					`,
+				}}
+			</script>
 			<title>{title}</title>
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
