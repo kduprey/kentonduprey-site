6273db5ec6371ff1c92884cf25f3afeb5ea2f53e
Kenton Duprey <kentonduprey@icloud.com>
2021-12-02 23:32:05 +0000

    integrated icon component with skill component

--- pages/components/Skill.js
+++ pages/components/Skill.js
@@ -1,9 +1,17 @@
-const Skill = ({ title, children }) => {
+import Icon from "./Icon";
+
+const Skill = ({ project, title, iconName, children }) => {
 	if (!title) title = "Skill Title";
+
+	if (project) {
+		return <Icon iconName={iconName} />;
+	}
 
 	return (
 		<div className="container p-3 flex flex-col items-center justify-center w-auto">
-			<div className="pb-3 text-5xl">{children}</div>
+			<div className="pb-3 text-5xl">
+				<Icon iconName={iconName} />
+			</div>
 			<h1 className="text-2xl font-semibold text-center">{title}</h1>
 		</div>
 	);
