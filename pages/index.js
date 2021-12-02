673ce8be774add3e7e99a3ef995a758aa799175c
Kenton Duprey <kentonduprey@icloud.com>
2021-12-02 14:13:19 +0000

    adding bio cms query

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,24 @@ import Contact from "./components/Contact";
 import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
 import { SiNextdotjs, SiAzurefunctions, SiMongodb } from "react-icons/si";
 import { GrGraphQl } from "react-icons/gr";
+import axios from "axios";
 
-const Home = () => {
+const QUERY = `{
+	bios(where: {createdBy: {name: "Kenton Duprey"}}) {
+	  biographyBlurb {
+		text
+	  }
+	  bioPic {
+		url
+		width
+		height
+	  }
+	}
+  }
+  `;
+
+const Home = ({ bioInfo }) => {
+	console.log(bioInfo);
 	return (
 		<div>
 			{/* Header */}
@@ -36,7 +52,12 @@ const Home = () => {
 							]}
 						/>
 					</div>
-					<About blurb="Hi, my name is Kenton and I am a student at the University of Westmnster persuing a degree in BSc in Computer Science. I am very passionate about web applications and security along with creating elegant designs with the latest web technlogies. I am currently in my final year of studies and looking forward to taking my knowledge to my field and being able to grow more and learn more as a developer in my field." />
+					<About
+						blurb={bioInfo.biographyBlurb.text}
+						imageUrl={bioInfo.bioPic.url}
+						imageHeight={bioInfo.bioPic.height}
+						imageWidth={bioInfo.bioPic.width}
+					/>
 					<section className="mx-auto md:w-4/5 lg:w-auto">
 						<h1 className="text-5xl font-bold text-center py-3">
 							Skills
@@ -73,3 +94,28 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getStaticProps(context) {
+	try {
+		const { data } = await axios.get(
+			process.env.NEXT_PUBLIC_GRAPH_CMS_API,
+			{
+				params: {
+					query: QUERY,
+				},
+			}
+		);
+		return {
+			props: {
+				bioInfo: data.data.bios[0],
+			},
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			props: {
+				error: "Error retreiving data from CMS",
+			},
+		};
+	}
+}
