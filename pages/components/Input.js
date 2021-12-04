fbc24c0fece3ee0dd35b34e43977a4a531d8eb0c
Kenton Duprey <kentonduprey@icloud.com>
2021-12-04 15:50:47 +0000

    adding input component

---
+++ pages/components/Input.js
@@ -0,0 +1,17 @@
+const Input = ({ error, textArea, classes, ...args }) => {
+	const inputClasses =
+		"ml-2 mt-2 bg-gray-100 border-transparent rounded transition ease-in focus:bg-white focus:border-gray-500 " +
+		classes +
+		(textArea
+			? " w-4/5 sm:w-3/5 md:w-2/5 lg:w-1/3 "
+			: " w-4/5 sm:w-3/5 md:w-2/5 lg:w-1/3 ") +
+		(error ? " border-red-500" : "");
+
+	if (textArea) {
+		return <textarea rows="3" className={inputClasses} {...args} />;
+	} else {
+		return <input className={inputClasses} {...args} />;
+	}
+};
+
+export default Input;
