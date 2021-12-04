f524b23cafa586d0767f3bb990a224efe0de4c17
Kenton Duprey <kentonduprey@icloud.com>
2021-12-04 15:51:18 +0000

    adding input tags on contact component

--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -1,4 +1,6 @@
-import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import Socials from "./Socials";
+import Button from "./Button";
+import Input from "./Input";
 
 const Contact = (props) => {
 	return (
@@ -7,28 +9,18 @@ const Contact = (props) => {
 			id="contact"
 		>
 			<h1 className="text-5xl font-bold text-center py-3">Contact</h1>
-			<div>
-				<p className="font-light text-xl">
-					Email:
-					<a
-						href="mailto:kentonduprey@icloud.com"
-						className="text-xl font-bold text-center underline px-2 hover:text-gray-700 transition-colors ease-in"
-					>
-						kentonduprey@icloud.com
-					</a>
-				</p>
-			</div>
-			<div className="flex justify-center items-center py-3">
-				<a href="https://github.com/kentond18" className="p-2">
-					<FaGithub className="text-4xl hover:text-gray-700 transition-colors ease-in" />
-				</a>
-				<a href="https://www.instagram.com/kentond18/" className="p-2">
-					<FaInstagram className="text-4xl hover:text-gray-700 transition-colors ease-in" />
-				</a>
-				<a href="https://twitter.com/kentond18" className="p-2">
-					<FaTwitter className="text-4xl hover:text-gray-700 transition-colors ease-in" />
-				</a>
-			</div>
+			<form className="flex flex-col items-center container">
+				<div className="container flex flex-col md:flex-row items-center justify-center">
+					<Input type="text" placeholder="Name" />
+					<Input type="email" placeholder="Email" />
+				</div>
+
+				<Input textArea={true} placeholder="Message" />
+				<Button type="submit" className="w-1/6 m-2">
+					Send
+				</Button>
+			</form>
+			<Socials />
 		</div>
 	);
 };
