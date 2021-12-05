5e10450caabcddb98243f929924d36460d3e0e4c
Kenton Duprey <kentonduprey@icloud.com>
2021-12-05 15:19:14 +0000

    removed container on main tag

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ const Home = ({ bioInfo, projects, skills }) => {
 			{/* Header */}
 			<Header title="Kenton Duprey - Web Developer" />
 
-			<main className="font-display vh-full flex flex-col items-center justify-center container">
+			<main className="font-display vh-full flex flex-col items-center justify-center">
 				<Navbar />
 				<Hero blurb="" />
 				<section className="container md:w-4/5" id="work">
