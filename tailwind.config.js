b6632830b1c8e6aba108acc8185eb9c443ea5138
Kenton Duprey <kentonduprey@icloud.com>
2021-12-16 21:33:27 -0700

    removed bootstrap screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		screens: {
-			sm: "576px",
-			md: "768px",
-			lg: "992px",
-			xl: "1200px",
-			xxl: "1400px",
-		},
 		fontFamily: {
 			display: ["Inter", "sans-serif"],
 		},
