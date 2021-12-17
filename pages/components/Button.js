477691af27fc8c14383cc2bcb5e33fb34060ce50
Kenton Duprey <kentonduprey@icloud.com>
2021-12-16 21:36:40 -0700

    changed hover bg gray color

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -3,7 +3,7 @@ const Button = ({ className, children, ...attributes }) => {
 		<button
 			className={
 				className +
-				" flex flex-row justify-evenly m-1 font-display items-center py-1 px-3 focus:outline-none rounded text-base bg-black text-white hover:bg-gray-700 transition ease-in dark:border-gray-300 dark:border"
+				" flex flex-row justify-evenly m-1 font-display items-center py-1 px-3 focus:outline-none rounded text-base bg-black text-white hover:bg-gray-800 transition ease-in dark:border-gray-300 dark:border"
 			}
 			{...attributes}
 		>
