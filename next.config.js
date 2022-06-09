9a457cf60d7caedd6739f5cbaae842ed8da5b35e
Kenton Duprey <kentonduprey@icloud.com>
2022-06-09 14:49:53 -0400

    Adding resume and redirect to CV

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
 	images: {
 		domains: ["via.placeholder.com"],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/resume",
+				destination: "/Kenton-Duprey.pdf",
+				type: "permanent",
+			},
+		];
+	},
 };
