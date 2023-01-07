cdafc610b13b08376823705a56305756a82dd409
Kenton Duprey <kentonduprey@icloud.com>
2023-01-07 01:10:13 -0500

    Silencing TS errors on Icon component

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as FontAwesome from "react-icons/fa";
-import * as SimpleIcons from "react-icons/si";
 import * as GrommetIcons from "react-icons/gr";
+import * as SimpleIcons from "react-icons/si";
 import { IconName } from "../types";
 
 type Props = {
@@ -16,18 +16,22 @@ const Icon = ({ iconName, size, color }: Props) => {
 	const library = iconName.icon.substring(0, 2).toLowerCase();
 	switch (library) {
 		case "fa":
+			// @ts-ignore
 			const a = React.createElement(FontAwesome[iconName.icon]);
 			return <div style={{ fontSize: size, color: color }}>{a}</div>;
 			break;
 		case "si":
+			// @ts-ignore
 			const b = React.createElement(SimpleIcons[iconName.icon]);
 			return <div style={{ fontSize: size, color: color }}>{b}</div>;
 			break;
 		case "gr":
+			// @ts-ignore
 			const c = React.createElement(GrommetIcons[iconName.icon]);
 			return <div style={{ fontSize: size, color: color }}>{c}</div>;
 			break;
 		default:
+			// @ts-ignore
 			const d = React.createElement(FontAwesome[iconName.icon]);
 			return <div style={{ fontSize: size, color: color }}>{d}</div>;
 			break;
