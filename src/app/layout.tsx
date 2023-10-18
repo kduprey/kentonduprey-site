6b50239f76d5cc8e1ff3ee7df38c6dcad11c9bb9
Kenton Duprey <kentonduprey@icloud.com>
2023-10-17 23:56:19 -0400

    Fixing font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,22 @@
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "Kenton Duprey - Web Developer",
 	description: "Building elegant web solutions for clients and companies",
 };
 
+const inter = Inter({
+	subsets: ["latin"],
+	style: ["normal"],
+	variable: "--font-raleway",
+});
+
 const rootLayout = ({ children }: PropsWithChildren) => {
 	return (
-		<html lang="en">
+		<html lang="en" className={`${inter.variable}`}>
 			<body>{children}</body>
 		</html>
 	);
