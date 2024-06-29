8ba79f8a3bca1a048acf962da1590fb55ca31af5
Kenton Duprey <kentonduprey@icloud.com>
2024-06-29 18:20:40 -0400

    refactor: Update sms route to remove error handling

--- apps/frontend/src/app/api/sms/route.ts
+++ apps/frontend/src/app/api/sms/route.ts
@@ -1,17 +1,17 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-	if (error) {
-		console.error(error);
-		return new NextResponse("Error message not set", {
-			status: 500,
-		});
-	}
+  // if (error) {
+  // 	console.error(error);
+  // 	return new NextResponse("Error message not set", {
+  // 		status: 500,
+  // 	});
+  // }
 
-	return NextResponse.json(
-		{},
-		{
-			status: 200,
-		}
-	);
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+    },
+  );
 };
