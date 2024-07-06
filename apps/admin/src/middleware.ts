d8f7082d05e15d42b310c7cf6b1cd62bdad9f00e
Kenton Duprey <kentonduprey@icloud.com>
2024-07-05 20:45:09 -0400

    feat: Update clerkMiddleware to include additional protection logic

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -3,7 +3,17 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  const { userId } = auth();
+  if (isProtectedRoute(req)) {
+    auth().protect(
+      () => {
+        return userId === process.env.ADMIN_USER_ID;
+      },
+      {
+        unauthorizedUrl: new URL("/unauthorized", req.url).toString(),
+      },
+    );
+  }
 });
 
 export const config = {
