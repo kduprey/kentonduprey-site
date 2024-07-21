323c2c9f2790f463ca5b47ff69a859402d6b5bc6
Kenton Duprey <kentonduprey@icloud.com>
2024-07-21 13:20:02 -0400

    refactor: Update middleware to handle CORS and JWT authentication
    This commit refactors the middleware in `middleware.ts` to handle Cross-Origin Resource Sharing (CORS) and JSON Web Token (JWT) authentication.
    
    - Added CORS handling to allow requests from specified origins
    - Implemented JWT verification using the `jsonwebtoken` library
    - Added dependencies for `jose` and `jwks-rsa` to handle JWT decoding and key retrieval
    - Extracted JWT verification logic into a separate function `checkJWT`
    - Updated the middleware configuration to include additional API routes for meter events and SMS

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,25 +1,80 @@
-import { NextResponse, type NextRequest } from "next/server";
+import {
+  NextResponse,
+  type MiddlewareConfig,
+  type NextRequest,
+} from "next/server";
 import { verify } from "jsonwebtoken";
+import { SERVER_SITE_URL } from "@kduprey/config";
 import { z } from "zod";
+import { JwksClient } from "jwks-rsa";
+import { decodeProtectedHeader } from "jose";
 
-export const config = {
-  matcher: "/api/stripe/billing/meter_events/(.*)",
+export const config: MiddlewareConfig = {
+  matcher: ["/api/stripe/billing/meter_events/(.*)", "/api/sms/(.*)"],
 };
 
-export function middleware(request: NextRequest) {
-  const secret = z.string().parse(process.env.JWT_SECRET);
-  // Check if the request has a cookie with the name "session"
+const allowedOrigins = [SERVER_SITE_URL];
+
+const corsOptions = {
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+};
+
+export async function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get("origin") ?? "";
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+  console.info("origin", origin, "isAllowedOrigin", isAllowedOrigin);
+
+  // Handle preflighted requests
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  } else {
+    response.headers.set("Access-Control-Allow-Origin", "null");
+    // Block the request
+    return new NextResponse(null, {
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigins.join(", "),
+        ...corsOptions,
+      },
+      status: 403,
+      statusText: "Forbidden",
+    });
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  // Check if the request has a header with the JWT
   const authJWT = request.headers.get("Authorization");
 
-  // If the cookie is not present, return false
+  // If the JWT is not present, return 401 with an error message
   if (!authJWT) {
     console.error("No JWT present in request");
-    return false;
+    return new NextResponse(null, {
+      status: 401,
+      statusText: "Unauthorized",
+    });
   }
 
   try {
     // If the cookie is present, verify the JWT
-    verify(authJWT, secret);
+    await checkJWT(authJWT);
   } catch (error) {
     // If the JWT is invalid, return 401 with an error message
     return Response.json(error, { status: 401, statusText: "Unauthorized" });
@@ -27,3 +82,48 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+const checkJWT = async (authJWT: string) => {
+  const jwksUri = `https://${z
+    .string({
+      message: "Missing AUTH0_DOMAIN environment variable",
+    })
+    .parse(process.env.AUTH0_DOMAIN)}/.well-known/jwks.json`;
+
+  const jwksClient = new JwksClient({
+    jwksUri,
+    requestHeaders: {},
+    jwksRequestsPerMinute: 5,
+    rateLimit: true,
+    cache: true,
+    timeout: 30000,
+  });
+
+  const secret = await jwksClient.getSigningKey(
+    decodeProtectedHeader(authJWT).kid,
+  );
+
+  verify(
+    authJWT,
+    secret.getPublicKey,
+    {
+      audience: z
+        .string({
+          message: "Missing AUTH0_AUDIENCE environment variable",
+        })
+        .parse(process.env.AUTH0_AUDIENCE),
+      issuer: `https://${z
+        .string({
+          message: "Missing AUTH0_DOMAIN environment variable",
+        })
+        .parse(process.env.AUTH0_DOMAIN)}/`,
+      algorithms: ["RS256"],
+    },
+    (err) => {
+      if (err) {
+        return { error: err };
+      }
+      return true;
+    },
+  );
+};
