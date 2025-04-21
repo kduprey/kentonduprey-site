c485b3ccf17d69f94967ee991acea671e3a8060d
Kenton Duprey <kenton@hausofweb.com>
2025-04-20 23:12:49 -0400

    chore(frontend): update Sentry configuration and package.json settings
    - Added `SENTRY_URL` environment variable to major, minor, and patch release workflows.
    - Updated `package.json` to specify built dependencies for pnpm.
    - Changed Sentry organization and URL in frontend configuration files.
    - Replaced hardcoded DSN values with environment variable in Sentry client and server configurations.
    
    Signed-off-by: Kenton Duprey <kenton@hausofweb.com>

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -41,9 +41,9 @@ module.exports = withSentryConfig(module.exports, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "kenton-duprey",
+  org: "haus-of-web",
   project: "kentonduprey-site",
-
+  sentryUrl: "https://sentry.hausofweb.com",
   // Only print logs for uploading source maps in CI
   silent: true,
 
@@ -58,9 +58,6 @@ module.exports = withSentryConfig(module.exports, {
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: false,
