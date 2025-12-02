e9b11a3a099696fd506a6ff0325f6591d3770561
Kenton Duprey <kenton@hausofweb.com>
2025-12-02 13:20:14 -0500

    fix(prisma): update Prisma client configuration and add PostgreSQL adapter
    Signed-off-by: Kenton Duprey <kenton@hausofweb.com>

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,27 +1,35 @@
-import { type Prisma, PrismaClient } from "@prisma/client";
+import { env } from "prisma/config";
+import { PrismaClient, Prisma } from "./generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
 
 export * from "@prisma/client";
 export { prisma };
+const connectionString = env("DATABASE_URL");
 
-export const prismaConfig: Prisma.PrismaClientOptions =
+export const prismaConfig: Prisma.PrismaClientOptions=
   process.env.NODE_ENV === "production"
     ? {
+    adapter: new PrismaPg({ connectionString }),
+
         log: ["query", "info", "warn", "error"],
         errorFormat: "minimal",
       }
     : {
-        log: ["query", "info", "warn", "error"],
+    adapter: new PrismaPg({ connectionString }),
+    log: ["query", "info", "warn", "error"],
         errorFormat: "pretty",
       };
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ ...prismaConfig });
+  return new PrismaClient({ 
+   ...prismaConfig
+   });
 };
 
 declare const globalThis: {
-  prismaGlobal?: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal?: PrismaClient;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma: PrismaClient = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
